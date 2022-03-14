e11a4ddac5bc5154453fcc6e27688cd1a2b84e4f
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-03-14 12:31:34 +0100

    added test db to resources

--- src/test/java/rest/HobbyResourceTest.java
+++ src/test/java/rest/HobbyResourceTest.java
@@ -1,12 +1,22 @@
 package rest;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManagerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HobbyResourceTest {
+    private static EntityManagerFactory emf;
+
+    @BeforeAll
+    public static void setUpClass() {
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+    }
 
     @BeforeEach
     void setUp() {
--- src/test/java/rest/PersonResourceTest.java
+++ src/test/java/rest/PersonResourceTest.java
@@ -1,12 +1,20 @@
 package rest;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utils.EMF_Creator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.persistence.EntityManagerFactory;
 
 class PersonResourceTest {
+    private static EntityManagerFactory emf;
+
+    @BeforeAll
+    public static void setUpClass() {
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+    }
 
     @BeforeEach
     void setUp() {
